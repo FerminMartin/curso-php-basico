dcea016a75342256436fb31a4a8b157d80dac236
fermin <fermartorr01@gmail.com>
2023-03-30 23:04:56 +0200

    Añado la clase de sintaxis básica, debugging y comentarios y la de variables

---
+++ index.php
@@ -0,0 +1,15 @@
+<!-- 
+El código que venía en index.html (que borré):
+<html><body><h1>It works!</h1></body></html> 
+-->
+<html>
+  
+<body>
+
+<h1>It works!</h1>
+<?php $msg = "Holi, mundo";
+
+echo $msg;
+?>
+
+</body></html>
