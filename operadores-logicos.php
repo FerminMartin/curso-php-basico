e9cdf9787c382f19b9171f6a4a965cab0c6fc60f
fermin <fermartorr01@gmail.com>
2023-03-31 15:04:03 +0200

    Operadores lógicos. Qué son las tablas de la verdad

---
+++ operadores-logicos.php
@@ -0,0 +1,24 @@
+<?php
+
+$michis_felinos = true;
+$michis_4_patas = true;
+$michis_vuelan = false;
+$michis_programan_con_PHP = false;
+
+// And
+
+// var_dump($michis_felinos && $michis_4_patas);
+
+// Or
+// var_dump($michis_4_patas || $michis_vuelan);
+
+//not
+// var_dump(!$michis_4_patas );
+
+//Experimentos que te va a hacer decir whaaat
+
+$resultado = $michis_4_patas and $michis_programan_con_PHP;
+var_dump($resultado);
+
+
+echo"\n";
