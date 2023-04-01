1041f6b348a1061c694a40a4cc25970ee624c590
fermin <fermartorr01@gmail.com>
2023-04-01 07:30:02 +0200

    Otros operadores(asignación, incremento/decremento(suma, resta, multiplicación y división), concatenación)

---
+++ otros-operadores.php
@@ -0,0 +1,41 @@
+<?php
+
+// operador de asignación
+// $edad_de_jaimito = ($edad_de_pepito = 18) +5 ;
+
+// echo "La edad de Pepito es $edad_de_pepito \n";
+// echo "La edad de Jaimito es $edad_de_jaimito \n";
+
+// Operadores de incremento
+
+$contador = 1;
+
+// $contador = $contador + 1;
+// $contador += 1;
+// $contador++;
+
+/*
+
++=
+-=
+*=
+/=
+
+ */
+
+// echo $contador;
+
+// $nombre = "Carlos";
+// $nombre .= " " . "Santana";
+
+// echo $nombre;
+
+$contador = 1;
+$resultado = $contador++;
+
+echo $contador . "\n";
+echo $resultado;
+
+
+
+echo "\n";
